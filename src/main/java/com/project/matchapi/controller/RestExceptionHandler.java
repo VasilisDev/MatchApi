ef27fa793c8b81e74a7a5dfd6189bafe95287c7c
Vasilis <vatsakiris@gmail.com>
2025-08-03 22:41:10 +0300

    remove unused import

--- src/main/java/com/project/matchapi/controller/RestExceptionHandler.java
+++ src/main/java/com/project/matchapi/controller/RestExceptionHandler.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Objects;
-
 @RestControllerAdvice
 public class RestExceptionHandler {
 
